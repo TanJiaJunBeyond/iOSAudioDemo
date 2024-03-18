5e35935988afe6b064ff96767ae399198aa6d765
谭嘉俊 <1120571286@qq.com>
2024-03-19 02:43:17 +0800

    优化代码。

--- iOSAudioDemo/ContentView.swift
+++ iOSAudioDemo/ContentView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // 调用get_lame_version函数就能获取当前LAME版本
     let lameVersion: String = String(cString: get_lame_version())
     
     var body: some View {
         NavigationView {
             Text(lameVersion)
-                .padding()
                 .navigationTitle("iOSAudioDemo")
         }
     }
